4e7b0c1e62f6c63f3e0737bfad3976a18727f24a
Mary Nguyen <156857000+MaryN26@users.noreply.github.com>
2026-02-03 18:28:43 -0600

    implement Intake subsystem with motor configuration for intake wheels

---
+++ src/main/java/frc/robot/subsystems/swervedrive/Intake.java
@@ -0,0 +1,52 @@
+package frc.robot.subsystems.swervedrive;
+
+ import java.security.PrivateKey;
+ 
+ import com.revrobotics.RelativeEncoder;
+ import com.revrobotics.spark.SparkBase.PersistMode;
+ import com.revrobotics.spark.SparkBase.ResetMode;
+ import com.revrobotics.spark.SparkLowLevel.MotorType;
+ import com.revrobotics.spark.SparkMax;
+ import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+ import com.revrobotics.spark.config.SparkMaxConfig;
+
+ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+ import edu.wpi.first.wpilibj2.command.Command;
+ import edu.wpi.first.wpilibj2.command.InstantCommand;
+ import edu.wpi.first.wpilibj2.command.RunCommand;
+ import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+public class Intake {
+    //create a new variable for motor 9
+    private SparkMax m_motor_9;
+
+    //this method controls the intake wheels 
+    public Intake() {
+        //initialize motor 9 as a SparkMax motor
+        m_motor_9 = new SparkMax(9, MotorType.kBrushless);
+
+        //set up configs for SparkMax motors
+        SparkMaxConfig global_config = new SparkMaxConfig();
+        SparkMaxConfig motor_9_config = new SparkMaxConfig();
+
+        //configure motor settings
+        global_config
+            //only use 40 if NOT 550 
+            .smartCurrentLimit(40);
+            //learn more about idle mode in the API docs
+            .idleMode(IdleMode.kBrake);
+            //learn more about persist mode in the API docs
+            .persistMode(PersistMode.kPersist);
+
+        motor_9_config
+            .apply(global_config);
+
+        m_motor_9.configure(motor_9_config,ResetMode.kResetSafeParameters,PersistMode.kPersistParameters);
+
+    }
+    
+    public Arm(){
+        
+    }
+}
