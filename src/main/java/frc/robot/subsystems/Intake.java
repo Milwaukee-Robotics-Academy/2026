889cc385d2140240af12815ebbad93fd2e17d8ac
lorenamartinezpajaro <156966834+lorenamartinezpajaro@users.noreply.github.com>
2026-02-05 17:35:26 -0600

    Refactor Intake subsystem: reintroduce motor configurations and control methods for intake and arm

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,27 +1,26 @@
-package frc.robot.subsystems.swervedrive;
+package frc.robot.subsystems;
 
- import java.security.PrivateKey;
- 
- import com.revrobotics.RelativeEncoder;
- import com.revrobotics.spark.SparkBase.PersistMode;
- import com.revrobotics.spark.SparkBase.ResetMode;
- import com.revrobotics.spark.SparkLowLevel.MotorType;
- import com.revrobotics.spark.SparkMax;
- import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
- import com.revrobotics.spark.config.SparkMaxConfig;
+import java.security.PrivateKey;
 
- import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
- import edu.wpi.first.wpilibj2.command.Command;
- import edu.wpi.first.wpilibj2.command.InstantCommand;
- import edu.wpi.first.wpilibj2.command.RunCommand;
- import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake {
-    //create a new variable for motor 9
+    // create a new variable for motor 9
     private SparkMax m_motor_9;
 
-    //this method controls the intake wheels 
+    // this method controls the intake wheels
     public Intake() {
         //initialize motor 9 as a SparkMax motor
         m_motor_9 = new SparkMax(9, MotorType.kBrushless);
@@ -45,7 +44,7 @@ public class Intake {
         m_motor_9.configure(motor_9_config,ResetMode.kResetSafeParameters,PersistMode.kPersistParameters);
 
     }
-    
+
     public Arm(){
          //initialize motor 10 as a SparkMax motor
         m_motor_10 = new SparkMax(10, MotorType.kBrushless);
@@ -67,35 +66,35 @@ public class Intake {
             .apply(global_config);
 
         m_motor_10.configure(motor_10_config,ResetMode.kResetSafeParameters,PersistMode.kPersistParameters);    
-    }
-    private void unlockAlgae()
-   {
-     intakeState = intakeStates.UNLOCKED;
-     encoderLockValue = 0;
-   }
- 
-   private void intake(){
-     unlockAlgae();
-     m_motor_9.set(0.5);
-   }
- 
-   private void stopIntake(){
-     m_motor_9.set(0);
-   }
- 
-   private void reverse(){
-     unlockAlgae();
-     m_motor_9.set(-0.5);
-   }
- 
-   private void raiseArm(){
-     m_motor_10.set(0.25);
-   }
-   public void stopArm(){
-    m_motor_10.set(0);
-  }
-  public void lowerArm(){
-    m_motor_10.set(-0.25);
-  }
-}
+    }private void()
 
+    {
+        intakeState = intakeStates.UNLOCKED;
+        encoderLockValue = 0;
+    }
+
+    // methods to control the intake wheels
+    private void forwardIntake() {
+        m_motor_9.set(0.5);
+    }
+    private void reverseIntake() {
+        m_motor_9.set(-0.5);
+    }
+    private void stopIntake() {
+        m_motor_9.set(0);
+    }
+
+
+    // methods to control the arm
+    public void lowerArm() {
+        m_motor_10.set(-0.25);
+    }
+
+    private void raiseArm() {
+        m_motor_10.set(0.25);
+    }
+
+    public void stopArm() {
+        m_motor_10.set(0);
+    }
+}
