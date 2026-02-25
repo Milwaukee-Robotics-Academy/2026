4fd7f6a0b96d278b195372fc5184d87203ac29b8
Mary Nguyen <156857000+MaryN26@users.noreply.github.com>
2026-02-25 16:36:25 -0600

    changed arm speed from -.3 to -.2

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -41,7 +41,7 @@ public class Intake extends SubsystemBase{
     private static final double INTAKE_SPEED_FORWARD = 0.5; 
     private static final double INTAKE_SPEED_REVERSE = -0.5; 
 
-    private static final double ARM_SPEED_MOVE_UP = -0.3;   //up = negative (arm is inverted)
+    private static final double ARM_SPEED_MOVE_UP = -0.2;   //up = negative (arm is inverted)
     private static final double ARM_SPEED_MOVE_DOWN = 0.1;  //down = positive
 
     // ==================== CONSTRUCTOR (CONFIGURE MOTORS) ====================
