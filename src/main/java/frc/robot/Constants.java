56da9caa3183dda2dafc7fbcd53ab8d6bc27e8f4
Laptop user <laptop@milwaukeeroboticsacademy.org>
2026-03-08 14:45:47 -0500

    Refactor vision subsystem to enhance pose estimation and integrate camera configurations

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,7 +4,15 @@
 
 package frc.robot;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.math.util.Units;
 import swervelib.math.Matter;
 
@@ -39,6 +47,35 @@ public final class Constants
 
     // Hold time on motor brakes when disabled
     public static final double WHEEL_LOCK_TIME = 10; // seconds
+  }
+
+  //Starting out with last year's data
+    public static class Vision {
+
+    public static final String kCameraNameFrontLeft = "Arducam_OV9281_USB_Camera";
+    public static final String kCameraNameRearRight = "Arducam_OV9782_USB_Camera (1)";
+    // Cam mounted facing forward, 11 inches forward of center, 9 inches left of center, 8 inches up
+    // from center.
+    public static final Transform3d kRobotToCamFrontLeft = new Transform3d(
+      new Translation3d(
+          Units.inchesToMeters(11.5), 
+          Units.inchesToMeters(12.5), 
+          Units.inchesToMeters(8)),
+      new Rotation3d(0, 0, Units.degreesToRadians(-15)));
+    public static final Transform3d kRobotToCamRearRight = new Transform3d(
+      new Translation3d(
+        Units.inchesToMeters(-11.75), 
+        Units.inchesToMeters(-9), 
+        Units.inchesToMeters(8)),
+        new Rotation3d(0, 0, Units.degreesToRadians(172)));
+    // The layout of the AprilTags on the field
+    public static final AprilTagFieldLayout kTagLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2026RebuiltAndymark);
+
+    // The standard deviations of our vision estimated poses, which affect
+    // correction rate
+    // (Fake values. Experiment and determine estimation noise on an actual robot.)
+    public static final Matrix<N3, N1> kSingleTagStdDevs = VecBuilder.fill(8, 8, 12);
+    public static final Matrix<N3, N1> kMultiTagStdDevs = VecBuilder.fill(5, 5, 10);
   }
 public static final class FuelConstants {
     // Motor controller IDs for Fuel Mechanism motors
